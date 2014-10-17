6b7ed6aafa15eaa33b49dc294f5201638e0d6874
Léo Hetsch <leo.hetsch@gmail.com>
2014-10-17 14:13:17 +0200

    Created ArticleView and Article model

---
+++ Pancakes/Article.h
@@ -0,0 +1,13 @@
+//
+//  Article.h
+//  Pancakes
+//
+//  Created by Leo on 17/10/2014.
+//  Copyright (c) 2014 Gobelins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Article : NSObject
+
+@end
---
+++ Pancakes/Article.m
@@ -0,0 +1,13 @@
+//
+//  Article.m
+//  Pancakes
+//
+//  Created by Leo on 17/10/2014.
+//  Copyright (c) 2014 Gobelins. All rights reserved.
+//
+
+#import "Article.h"
+
+@implementation Article
+
+@end
