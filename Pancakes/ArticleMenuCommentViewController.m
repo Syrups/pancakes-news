eb49c962b7de4dac4548361cb02f7512cfb387b7
Léo Hetsch <leo.hetsch@gmail.com>
2014-11-18 20:47:24 +0100

    Comment sending

--- Pancakes/ArticleMenuCommentViewController.m
+++ Pancakes/ArticleMenuCommentViewController.m
@@ -8,6 +8,9 @@
 
 #import "ArticleMenuCommentViewController.h"
 #import "ArticleViewController.h"
+#import "Configuration.h"
+#import <JSONModel/JSONHTTPClient.h>
+#import "UserDataHolder.h"
 
 @interface ArticleMenuCommentViewController ()
 
@@ -35,6 +38,32 @@
     btn.tag = 10;
     
     [articleVc.menuViewController didSelectItem:btn];
+}
+
+- (IBAction)postComment:(UITextField*)sender {
+    
+//    NSString* userId = [[UserDataHolder sharedInstance] user]._id;
+    NSString* userId = @"5440e462d1e57b5861b503d3";
+    
+    ArticleViewController* articleVc = (ArticleViewController*)self.parentViewController.parentViewController;
+    
+    NSDictionary *params = @{
+        @"user_id": userId,
+        @"content": sender.text
+    };
+
+    //make post, get requests
+    [JSONHTTPClient postJSONFromURLWithString:[kApiRootUrl stringByAppendingString:[NSString stringWithFormat:@"/articles/%@/comments", articleVc.displayedArticle._id]]
+                                       params:params
+                                   completion:^(id json, JSONModelError *err) {
+                                       
+                                       NSLog(@"Response : %@", json);
+                                       
+                                       if (err == nil) {
+                                           [self dismiss:nil];
+                                       }
+                                       
+    }];
 }
 
 @end
